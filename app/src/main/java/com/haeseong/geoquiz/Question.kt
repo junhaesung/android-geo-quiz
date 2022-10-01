cca248ab3ba85fb2a96d6073939049c05a886cf8
Haeseong Jeon <seong0428@gmail.com>
2022-10-01 21:39:51 +0900

    질문 업데이트

---
+++ app/src/main/java/com/haeseong/geoquiz/Question.kt
@@ -0,0 +1,6 @@
+package com.haeseong.geoquiz
+
+data class Question(
+    val mTextResId: Int,
+    val mAnswerTrue: Boolean,
+)
--- app/src/main/java/com/haeseong/geoquiz/QuizActivity.kt
+++ app/src/main/java/com/haeseong/geoquiz/QuizActivity.kt
@@ -1,9 +1,8 @@
 package com.haeseong.geoquiz
 
 import android.os.Bundle
-import android.view.View
 import android.widget.Button
-import android.widget.Toast
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.snackbar.Snackbar
 
@@ -11,12 +10,28 @@ import com.google.android.material.snackbar.Snackbar
  * @see https://stackoverflow.com/a/60205538
  */
 class QuizActivity : AppCompatActivity() {
+
+    val mQuestions = listOf(
+        Question(R.string.question_oceans, true),
+        Question(R.string.question_mideast, false),
+        Question(R.string.question_africa, false),
+        Question(R.string.question_americas, true),
+        Question(R.string.question_asia, true),
+    )
+
     lateinit var mTrueButton: Button
     lateinit var mFalseButton: Button
+    lateinit var mNextButton: Button
+    lateinit var mQuestionTextView: TextView
+    private var mCurrentIndex = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz)
+
+        mQuestionTextView = findViewById(R.id.question_text_view)
+        updateQuestion()
+
         mTrueButton = findViewById(R.id.true_button)
         mTrueButton.setOnClickListener {
             Snackbar.make(
@@ -33,5 +48,17 @@ class QuizActivity : AppCompatActivity() {
                 Snackbar.LENGTH_SHORT,
             ).show()
         }
+        mNextButton = findViewById(R.id.next_button)
+        mNextButton.setOnClickListener {
+            mCurrentIndex += 1
+            mCurrentIndex %= mQuestions.size
+            updateQuestion()
+        }
+
+        updateQuestion()
+    }
+
+    private fun updateQuestion() {
+        mQuestionTextView.setText(mQuestions[mCurrentIndex].mTextResId)
     }
 }
